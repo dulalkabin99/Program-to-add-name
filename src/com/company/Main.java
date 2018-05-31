7f8930ca6e7256e4d8c3bb9dd8f15ed31a5a1413
Kabin Dulal <dulalkabin99@gmail.com>
2018-05-31 14:04:03 -0400

    First Program to add my name

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println( "Hello, World!" );
+        System.out.println("Today is May 21, 2018. I am alive!  And");
+        System.out.println ("My Name is Kabin");
+    }
+}
